f744ec8d37cfa37953de32c35ac0df7fa9b3d2c8
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2014-02-11 01:51:53 -0500

    Add sksconf and membership config files

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,20 @@
 # This class is called from sks
 #
 class sks::config {
+
+  file { '/etc/sks/sksconf':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('sks/sksconf.erb'),
+  }
+
+  file { '/etc/sks/membership':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('sks/membership.erb'),
+  }
 }
