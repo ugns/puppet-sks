52f32f4a8c834ca7f1cd08a37cbdd525aa5eb64f
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2014-02-19 23:23:56 -0500

    Update Beaker acceptance test spec

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -20,7 +20,52 @@ describe 'sks class' do
 
     describe service('sks') do
       it { should be_enabled }
-      it { should be_running }
+    end
+  end
+
+  context 'non-default parameters' do
+    it 'should work with no errors' do
+      pp = <<-EOS
+      class { 'sks':
+        recon_address  => ['10.10.10.10', '10.20.10.10'],
+        hkp_address    => ['10.10.10.10', '10.20.10.10'],
+        members        => [{
+          hostname => 'sks.example.net',
+          keyid    => '0xFEEDBEEF',
+        },{
+          hostname => 'sks.example.org',
+          email    => 'oscar@example.org',
+          keyid    => '0xABCDFE98',
+        },{
+          hostname => 'sks.example.com',
+        },{
+          hostname => 'keyserver.example.net',
+          port     => '11371',
+          admin    => 'John Doe',
+          email    => 'jdoe@example.com',
+          keyid    => '0xDEADBEEF',
+        },{
+          hostname => 'keyserver.example.org',
+          admin    => 'Jane Doe',
+        },{
+          hostname => 'keyserver.example.com',
+          admin    => 'J Edgar Hoover',
+          keyid    => '0x01234567',
+        }],
+      }
+      EOS
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    describe package('sks') do
+      it { should be_installed }
+    end
+
+    describe service('sks') do
+      it { should be_enabled }
     end
   end
 end
